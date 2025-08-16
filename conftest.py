04601a307726a29015072f6685a2165b4b9f56e3
Meetansh Kharbanda <kharbandameetansh999@gmail.com>
2025-08-16 14:29:11 -0400

    final version

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
-import pytest
+# import pytest
 
-@pytest.fixture(scope = "session")
+# @pytest.fixture(scope = "session")
+# def base_url():
+#     return "https://jsonplaceholder.typicode.com"
+
+
+import os, pytest
+
+@pytest.fixture(scope="session")
 def base_url():
-    return "https://jsonplaceholder.typicode.com"
+    return os.getenv("BASE_URL", "https://jsonplaceholder.typicode.com")
